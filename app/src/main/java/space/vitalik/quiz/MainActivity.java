cc9570ce1c7633e343404212feb89ffc6310bfaa
Vitalik_M <vitalik.mahovik@gmail.com>
2022-05-04 11:00:36 +0300

    гшщ

--- app/src/main/java/space/vitalik/quiz/MainActivity.java
+++ app/src/main/java/space/vitalik/quiz/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
 
-    0х
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
