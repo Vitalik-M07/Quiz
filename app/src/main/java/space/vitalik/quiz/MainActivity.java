59f990564956dd26be2938eb00145049c357d858
Vitalik_M <vitalik.mahovik@gmail.com>
2022-06-07 21:14:26 +0300

    209

--- app/src/main/java/space/vitalik/quiz/MainActivity.java
+++ app/src/main/java/space/vitalik/quiz/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
